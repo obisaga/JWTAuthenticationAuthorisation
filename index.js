b16f8696b290d3247be5680281f3db340e36ab06
obisaga <o.bisaga@gmail.com>
2023-10-18 19:11:56 +0200

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+// import express from 'express';
+// import 'dotenv/config';
+// import client from './db/db.js';
+// import studentRouter from './routes/student.js';
+// import tokenRouter from './routes/token.js';
+// import userRouter from './routes/user.js';
+
+
+
+
+// const app = express();
+// app.use(express.json());
+// app.use('/api/students', studentRouter);
+// app.use('/api/tokens', tokenRouter);
+// app.use('/api/users', userRouter);
+
+
+
+// const port = process.env.PORT || 3000;
+
+// client.on('connected', () => {
+//     app.listen(port, () => {
+//         console.log(`Server listening on port ${port}`)
+//     })
+// })
+
